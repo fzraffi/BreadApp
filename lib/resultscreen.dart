736bc424a16fde74bef1b7bf59f89d050a4330e7
faizal <faizalraffi898@gmail.com>
2023-11-30 10:18:18 +0700

    Perubahan Layout ResultScreen

--- lib/resultscreen.dart
+++ lib/resultscreen.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'main.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:stroke_text/stroke_text.dart';
 
 class ResultScreen extends StatelessWidget {
   final Bread selectedBread;
@@ -13,17 +15,17 @@ class ResultScreen extends StatelessWidget {
   final int burnPrice;
   final int totalPrice;
 
-  const ResultScreen({
-    Key? key,
-    required this.selectedBread,
-    required this.toppingOrNot,
-    required this.nameBread,
-    required this.selectedTopping,
-    required this.nameBurn,
-    required this.breadPrice,
-    required this.totalPrice,
-    required this.burnPrice
-  }) : super(key: key);
+  const ResultScreen(
+      {Key? key,
+      required this.selectedBread,
+      required this.toppingOrNot,
+      required this.nameBread,
+      required this.selectedTopping,
+      required this.nameBurn,
+      required this.breadPrice,
+      required this.totalPrice,
+      required this.burnPrice})
+      : super(key: key);
 
   String convertToppingToString(Topping? topping) {
     if (topping == null) {
@@ -81,103 +83,205 @@ class ResultScreen extends StatelessWidget {
             height: 70),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Padding(padding: EdgeInsets.only(top: 20)),
-          Text(
-            'Your Order Summary',
-            style: TextStyle(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(padding: EdgeInsets.only(top: 20)),
+            StrokeText(
+              text: 'Your Order Summary',
+              textStyle: TextStyle(
                 fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 84, 132, 231),
-                fontSize: 25),
-          ),
-          SizedBox(height: 20),
-          Padding(padding: EdgeInsets.all(20)),
-          Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color.fromARGB(255, 84, 132, 231)),
-            width: 300,
-            height: 200,
-            child: Column(
-              children: [
-                Padding(padding: EdgeInsets.all(10)),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text(
-                      '${nameBread}',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 15),
-                    ),
-                    Text(
-                      'Rp ${breadPrice}',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 20),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text(
-                      '${convertToppingToString(selectedTopping)}',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 15),
-                    ),
-                    Text(
-                      'Rp ${calculateToppingPrice(selectedTopping)}',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 20),
-                    ),
-                  ],
-                ),
-                                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text(
-                      '${nameBurn}',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 15),
-                    ),
-                    Text(
-                      'Rp ${burnPrice}',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 20),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      'TOTAL : ',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 30),
-                    ),
-                     Text('\Rp $totalPrice', style: TextStyle(fontSize: 30,
-                     color: Colors.white,
-                     fontWeight: FontWeight.bold)),
-                  ],
-                )
-              ],
+                fontSize: 25,
+                color: Colors.yellow,
+              ),
+              strokeColor: Colors.black,
+              strokeWidth: 1,
             ),
-          )
-        ],
+            SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color.fromARGB(255, 84, 132, 231)),
+              width: 350,
+              height: 200,
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: [
+                      Container(
+                        width: 125,
+                        height: 50,
+                        child: Center(
+                          child: Text(
+                            '${nameBread}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 15),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 50,
+                      ),
+                      Container(
+                        width: 125,
+                        height: 50,
+                        child: Center(
+                          child: Text(
+                            'Rp ${breadPrice}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 15),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 125,
+                        height: 50,
+                        child: Center(
+                          child: Text(
+                            '${convertToppingToString(selectedTopping)}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 15),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 50,
+                      ),
+                      Container(
+                        width: 125,
+                        height: 50,
+                        child: Center(
+                          child: Text(
+                            'Rp ${calculateToppingPrice(selectedTopping)}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 15),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 125,
+                        height: 50,
+                        child: Center(
+                          child: Text(
+                            '${nameBurn}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 15),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 50,
+                      ),
+                      Container(
+                        width: 125,
+                        height: 50,
+                        child: Center(
+                          child: Text(
+                            'Rp ${burnPrice}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 15),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 125,
+                        height: 50,
+                        child: Center(
+                          child: Text(
+                            'TOTAL',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 25),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 50,
+                      ),
+                      Container(
+                        width: 125,
+                        height: 50,
+                        child: Center(
+                          child: Text(
+                            'Rp ${totalPrice}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 25),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(padding: EdgeInsets.all(15)),
+            StrokeText(
+              text: 'Scan QR Code for Payment',
+              textStyle: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Colors.yellow,
+              ),
+              strokeColor: Colors.black,
+              strokeWidth: 1,
+            ),
+            QrImageView(
+              data: '''
+              Your Order:
+              ${nameBread}: Rp.${breadPrice}
+              ${convertToppingToString(selectedTopping)}: Rp.${calculateToppingPrice(selectedTopping)}
+              ${nameBurn}: Rp.${burnPrice}
+
+
+              Total: Rp.${totalPrice}
+              Thank you for your purchase
+              ''',
+              version: QrVersions.auto,
+              size: 150,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.all(12),
+        height: 50,
+        color: Color.fromARGB(255, 84, 132, 231),
+        alignment: Alignment.center,
+        child: Text(
+          'Get Problem? Contact us here',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
     );
   }
